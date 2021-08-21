dc33bf8d0cead2bf671b46bbdbe47a92386a6268
gorla1 <gorladineshyadav1@gmail.com>
2021-08-21 09:59:18 +0530

    REDUX footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Typography } from "@material-ui/core"
 class Footer extends React.Component {
     render() {
         return (
@@ -9,15 +10,15 @@ class Footer extends React.Component {
                     
                         
                         <div className="col" >
-                            <ul className="nav justify-content-center m-3">
-                                <li className="nav-item" style={{color:'white'}}>
-                                    <Link to='#' className="text-light">Terms</Link>
+                            <ul className="nav justify-content-center mt-4">
+                                <li className="nav-item " style={{color:'white', marginLeft:'3%', marginRight:'3%'}}>
+                                    <Link to='#' className="text-light"><Typography variant='h6'>Terms</Typography></Link>
                                 </li>
-                                <li className="nav-item">
-                                <Link to='#' className="text-light">Privacy</Link>
+                                <li className="nav-item" style={{color:'white', marginLeft:'3%', marginRight:'3%'}}>
+                                <Link to='#' className="text-light"><Typography variant='h6'>Privacy</Typography></Link>
                                 </li>
-                                <li className="nav-item">
-                                <Link to='https://github.com/gorla1/Product2/tree/gh-pages' className="text-light">G.DINESH</Link>
+                                <li className="nav-item" style={{color:'white', marginLeft:'3%', marginRight:'3%'}}>
+                                <a href="https://github.com/gorla1/Product2/tree/gh-pages" className="text-light"><Typography variant='h6'>G.DINESH</Typography></a>
                                 </li>
                             </ul>
 
